740d6f613a50d1adf8aae2a1eff37e5b67647eab
Macarius <49547750+MacariusD@users.noreply.github.com>
2022-04-20 20:52:18 -0500

    finishes repeat string tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(rSeq, xNum) {
+    let fSeq = ''
+    if (xNum < 0) {
+        fSeq = 'ERROR'
+        return fSeq;
+    } else {
+        for (let i = 0; i < xNum; i++) {
+            fSeq = fSeq + rSeq;
+        };
+        return fSeq;
+    };
 };
 
 // Do not edit below this line
