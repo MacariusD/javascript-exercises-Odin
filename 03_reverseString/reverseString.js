72c67dfacaeae4bedb841c67a31ccc36237bc46c
Macarius <49547750+MacariusD@users.noreply.github.com>
2022-04-20 21:31:57 -0500

    completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(rSeq) {
+    let seqArray = Array.from(rSeq);
+    let fSeq = '';
+    for (let i = seqArray.length; i > 0; i--) {
+        fSeq += seqArray[i-1];
+    };
+    return fSeq;
 };
 
 // Do not edit below this line
