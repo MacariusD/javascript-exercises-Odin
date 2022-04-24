f656927ee1c524c98961b8568e91618060597e44
Macarius <49547750+MacariusD@users.noreply.github.com>
2022-04-23 21:04:04 -0500

    completed remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+// ...sRemoveArgs = rest_parameters
+const removeFromArray = function(sArray, ...sRemoveArgs) {
+    const userArray = new Set(Array.from(sRemoveArgs));
+    const newArray = sArray.filter(x => !userArray.has(x));
+    return newArray;
 };
 
 // Do not edit below this line
